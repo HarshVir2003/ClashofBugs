450fabed7838d71d133981eb2bab13bd112734da
Jashanpreet Singh <120298510+Jass6767@users.noreply.github.com>
2024-02-24 12:34:42 +0530

    Update asgi.py

--- Backend/asgi.py
+++ Backend/asgi.py
@@ -1,5 +1,5 @@
 import os
-
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -10,6 +10,7 @@ from battle.routing import websocket_urlpatterns
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend.settings")
+django.setup()
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
